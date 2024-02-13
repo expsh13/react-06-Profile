59b97969f96f0ce0ace090159fc6a07c5f5bba35
Satoki Hata <satokihata@gmail.com>
2024-02-13 21:36:32 +0900

    update inputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { InputField } from "./components/InputField/InputField";
+
 export const App = () => {
   return (
     <div className="w-1/2 m-auto">
       <h1 className="font-bold text-center">プロフィール生成</h1>
       <div className="flex gap-2.5">
         <div className="w-2/5 flex flex-col gap-2">
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+          <InputField />
+          <InputField />
+          <InputField />
           <button>ダウンロード</button>
         </div>
         <div className="w-2/5 rounded shadow border-gray-400 border p-2">a</div>
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,11 @@
+export const InputField = () => {
+  return (
+    <div>
+      <p className="font-bold">text</p>
+      <input
+        type="text"
+        className="rounded shadow border-gray-400 border p-2"
+      />
+    </div>
+  );
+};

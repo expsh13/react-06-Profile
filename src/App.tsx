7f758023c933101d7d1ab831eeee46fe45c058b5
Satoki Hata <satokihata@gmail.com>
2024-02-14 08:45:45 +0900

    add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import { InputField } from "./components/InputField/InputField";
 
 export const App = () => {
+  const [name, setName] = useState("");
+  const [birthDay, setBirthDay] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <div className="w-1/2 m-auto">
       <h1 className="font-bold text-center">プロフィール生成</h1>
       <div className="flex gap-2.5">
         <div className="w-2/5 flex flex-col gap-2">
-          <InputField />
-          <InputField />
-          <InputField />
-          <button>ダウンロード</button>
+          <InputField
+            title="名前"
+            inputType="text"
+            onChange={(name: string) => setName(name)}
+          />
+          <InputField
+            title="誕生日"
+            inputType="date"
+            onChange={(date: string) => setBirthDay(date)}
+          />
+          <InputField
+            title="電話番号"
+            inputType="text"
+            onChange={(number: string) => setPhoneNumber(number)}
+          />
+          <button className="bg-green-500 text-gray-50 p-1 rounded font-bold">
+            ダウンロード
+          </button>
         </div>
-        <div className="w-2/5 rounded shadow border-gray-400 border p-2">a</div>
+        <div className="flex flex-col items-center w-2/5 rounded shadow border-gray-400 border p-2">
+          <p>{name}</p>
+          <p>{birthDay}</p>
+          <p>{phoneNumber}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,22 @@
-export const InputField = () => {
+import { ChangeEvent } from "react";
+
+type PropsType = {
+  title: string;
+  inputType: string;
+  onChange: (data: string) => void;
+};
+
+export const InputField = (props: PropsType) => {
+  const { title, inputType, onChange } = props;
   return (
     <div>
-      <p className="font-bold">text</p>
+      <p className="font-bold">{title}</p>
       <input
-        type="text"
-        className="rounded shadow border-gray-400 border p-2"
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          onChange(e.currentTarget.value)
+        }
+        type={inputType}
+        className="rounded shadow border-gray-400 border p-2 w-full block box-border"
       />
     </div>
   );

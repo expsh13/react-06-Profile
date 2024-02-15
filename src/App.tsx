b1c86b70dc21b8c1419a4eb369b97e7aec7bc5bc
Satoki Hata <satokihata@gmail.com>
2024-02-15 21:10:58 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import { InputField } from "./components/InputField/InputField";
 import { ImageUpload } from "./components/ImageUpload/ImageUpload";
+import html2canvas from "html2canvas";
 
 export const App = () => {
   const [name, setName] = useState("");
@@ -11,6 +12,25 @@ export const App = () => {
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.currentTarget.files && e.currentTarget.files[0]) {
       setImgUrl(URL.createObjectURL(e.currentTarget.files[0]));
+    }
+  };
+
+  const handleDownload = async () => {
+    console.log("koko");
+    const card = document.getElementById("profile-card");
+    if (card) {
+      const canvas = await html2canvas(card);
+      const imgData = canvas.toDataURL("image/png");
+      // ダウンロード用のリンク要素を作成
+      const downloadLink = document.createElement("a");
+      downloadLink.href = imgData;
+      // ダウンロードするファイル名を設定
+      downloadLink.download = "profile-card.png";
+      // リンクをクリックしてダウンロードを実行
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      // 不要になったリンク要素を削除
+      document.body.removeChild(downloadLink);
     }
   };
 
@@ -35,11 +55,17 @@ export const App = () => {
             onChange={(number: string) => setPhoneNumber(number)}
           />
           <ImageUpload title="プロフィール写真" onChange={handleFileChange} />
-          <button className="bg-green-500 text-gray-50 p-1 rounded font-bold hover:opacity-80">
+          <button
+            className="bg-green-500 text-gray-50 p-1 rounded font-bold hover:opacity-80"
+            onClick={handleDownload}
+          >
             ダウンロード
           </button>
         </div>
-        <div className="flex flex-col items-center gap-1 w-2/5 rounded shadow border-gray-400 border p-2">
+        <div
+          id="profile-card"
+          className="flex flex-col items-center gap-1 w-2/5 rounded shadow border-gray-400 border p-2"
+        >
           {imgUrl && <img src={imgUrl} />}
           <p>{name}</p>
           <p>{birthDay}</p>
